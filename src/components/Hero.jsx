bd7659217f8980bdf383a459a5f940cccbc320a8
Your Name <you@example.com>
2024-09-13 14:31:06 -0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/Abgyu", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        II SOOKOOB SIDA <br className='max-md:hidden' />
+        <span className='orange_gradient '>chatgpt oo kale </span>
+      </h1>
+      <h2 className='desc'>
+      wuxuu kuu aqrinayaa links  si add Summize ugu samaysid, soo koobid, sidoo kale maqaal il-furan oo maqaallo dhaadheer u rogaya qoraal cad oo kooban.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
